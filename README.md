29ba87372974a2111e7643cb9ed15e3041bfc3c4
Dulanka Nimsara <dulankanimsaradn@gmail.com>
2025-07-16 21:20:53 +0530

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# VELLARO System
-
--- footer.php
+++
@@ -1,5 +0,0 @@
-    <footer style="background-color: var(--dark-color); color: var(--light-color); text-align: center; padding: 1.5rem; margin-top: 2rem;">
-        <p>&copy; <?= date('Y') ?> VELLARO T-Shirt Management System. All rights reserved.</p>
-    </footer>
-</body>
-</html>
